693d0699732d9176749c937ea8a598cc3f0d9e2a
Izabela Kazana <izabelaxkazana@gmail.com>
2023-04-29 10:43:38 +0200

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -8,11 +8,14 @@ const ingredients = [
 ];
 
 const ingredientsList = document.querySelector('#ingredients')
+const list = [];
 
 ingredients.forEach(ingredient => {
-  const list = document.createElement('li');
-  list.textContent = ingredient;
-  ingredientsList.appendChild(list);
+  const liItem = document.createElement('li');
+  liItem.textContent = ingredient;
+  liItem.classList.add('item');
+  list.push(liItem);
 
 });
+ingredientsList.append(...list);
 console.log(ingredientsList)
