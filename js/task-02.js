aa4246099b05b8f5de2b2e285bd9cbdbc5a49ff7
Izabela Kazana <izabelaxkazana@gmail.com>
2023-05-12 09:40:56 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,18 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
 
-const ingredientsList = document.querySelector('#ingredients')
+const ingredientsList = document.querySelector("#ingredients");
 
-ingredients.forEach(ingredient => {
-  const list = document.createElement('li');
+const listItems = ingredients.map(ingredient => {
+  const list = document.createElement("li");
   list.textContent = ingredient;
-  ingredientsList.appendChild(list);
-
+  return list;
 });
-console.log(ingredientsList)
+
+ingredientsList.append(...listItems);
+
+console.log(ingredientsList);
